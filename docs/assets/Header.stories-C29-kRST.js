1e5757b3e183b78c94e5beff64a337f75f4db6f9
Paco Lara <paco@MacBook-Pro-de-Paco.local>
2026-02-27 19:55:44 +0100

    deploy storybook

---
+++ docs/assets/Header.stories-C29-kRST.js
@@ -0,0 +1,7 @@
+import{H as a}from"./Header-BGYvtqoh.js";import"./iframe-CGZd_-tf.js";import"./preload-helper-PPVm8Dsz.js";import"./Button-_JEi-w_d.js";const{fn:o}=__STORYBOOK_MODULE_TEST__,m={title:"Example/Header",component:a,tags:["autodocs"],parameters:{layout:"fullscreen"},args:{onLogin:o(),onLogout:o(),onCreateAccount:o()}},e={args:{user:{name:"Jane Doe"}}},r={};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  args: {
+    user: {
+      name: 'Jane Doe'
+    }
+  }
+}`,...e.parameters?.docs?.source}}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:"{}",...r.parameters?.docs?.source}}};const d=["LoggedIn","LoggedOut"];export{e as LoggedIn,r as LoggedOut,d as __namedExportsOrder,m as default};
